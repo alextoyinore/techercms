1bf2f8b91a341a5400cd0263a34f2a4e77db6aa6
Alexander Ore <aore8030@gmail.com>
2025-10-14 19:27:09 +0000

    Try fixing this error: `Runtime Error: Error: Unable to generate tokens.

--- src/app/config.ts
+++ src/app/config.ts
@@ -2,12 +2,26 @@ import {AuthOptions, ServiceAccount} from 'next-firebase-auth-edge';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
+
+function getServiceAccount(): ServiceAccount {
+  const privateKey = process.env.FIREBASE_PRIVATE_KEY;
+  if (!privateKey) {
+    throw new Error('FIREBASE_PRIVATE_KEY is not set');
+  }
+
+  return {
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
+    privateKey: Buffer.from(privateKey, 'base64').toString('utf-8'),
+  };
+}
+
 
 export const authConfig: AuthOptions = {
   apiKey: firebaseConfig.apiKey,
@@ -20,9 +34,5 @@ export const authConfig: AuthOptions = {
     sameSite: 'lax',
     maxAge: 12 * 60 * 60 * 24,
   },
-  serviceAccount: {
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY!,
-  },
+  serviceAccount: getServiceAccount(),
 };
