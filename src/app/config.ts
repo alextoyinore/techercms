495e2eaca64b13401b2d556a18b4f375e11caa20
Alexander Ore <aore8030@gmail.com>
2025-10-14 19:47:06 +0000

    Try fixing this error: `Runtime Error: Error: Unable to generate tokens.

--- src/app/config.ts
+++ src/app/config.ts
@@ -23,6 +23,8 @@ export const authConfig: AuthOptions = {
   serviceAccount: {
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY!,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY
+      ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+      : '',
   },
 };
