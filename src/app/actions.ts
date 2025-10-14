6417f8e42637e5732d89e1a2c91bc308aa66d63d
Alexander Ore <aore8030@gmail.com>
2025-10-14 19:26:36 +0000

    Try fixing this error: `Runtime Error: TypeError: this.cookies.get is no

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,10 +1,18 @@
 'use server';
 import {cookies} from 'next/headers';
-import {getTokens} from 'next-firebase-auth-edge';
+import {getTokens} from 'next-firebase-auth-edge/lib/next/tokens';
 import {authConfig} from './config';
 
 export async function sessionLogin(idToken: string) {
-  const tokens = await getTokens(idToken, authConfig);
+  const tokens = await getTokens(cookies(), {
+    ...authConfig,
+    idToken,
+  });
+
+  if (!tokens) {
+    throw new Error('Unable to generate tokens');
+  }
+
   cookies().set(tokens.cookieName, tokens.cookie, tokens.cookieOptions);
 }
 
