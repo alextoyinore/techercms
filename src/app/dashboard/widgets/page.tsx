6c0702a9ccc037d6e2a71096dc7467bba6b3d793
Alexander Ore <aore8030@gmail.com>
2025-10-15 02:16:54 +0000

    Try fixing this error: `Runtime Error: Error: useEffect is not defined.

--- src/app/dashboard/widgets/page.tsx
+++ src/app/dashboard/widgets/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useMemo, useState } from 'react';
+import { useMemo, useState, useEffect } from 'react';
 import { PageHeader } from "@/components/page-header";
 import {
     Card,
