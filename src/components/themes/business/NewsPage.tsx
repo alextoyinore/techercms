d86ede4340e92b7a33aad4d7a08d8824ef41cc88
Alexander Ore <aore8030@gmail.com>
2025-10-24 10:13:31 +0000

    Try fixing this error: `Runtime Error: Error: useEffect is not defined.

--- src/components/themes/business/NewsPage.tsx
+++ src/components/themes/business/NewsPage.tsx
@@ -1,6 +1,6 @@
 
 'use client';
-import { useMemo, useState } from 'react';
+import { useMemo, useState, useEffect } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { useFirestore, useCollection, useDoc, useMemoFirebase } from '@/firebase';
