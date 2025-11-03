7a6dd7062386bea7ca90ee266f4867723356adb3
Alexander Ore <aore8030@gmail.com>
2025-11-03 06:49:35 +0000

    title updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ import { TypographyProvider } from "@/components/TypographyProvider";
 import { GlobalStyleProvider } from "@/components/GlobalStyleProvider";
 
 export const metadata: Metadata = {
-  title: "95news",
+  title: "95News",
   keywords: "Breaking News, World News, Nigerian News, African News, Europe, Asia, North America, South America, US, Climate, Art, Culture, Music, Nigerian Music, Sports, Science, Tech, Innovation, Business, Markets, Photos, Video, Audio",
   description: "Breaking News, World News, Nigerian News, African News, Europe, Asia, North America, South America, US, Sport, Science, Tech, Innovation, Business, Markets, Video & Audio",
   verification: {
