2311173cea118ca62d2e689b717af4a2de2d4eb7
Alexander Ore <aore8030@gmail.com>
2025-11-04 00:10:42 +0000

    let's make loader page background white

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -4,7 +4,7 @@ import { Loader2 } from "lucide-react";
 
 export function Loading() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-background">
+    <div className="flex min-h-screen items-center justify-center bg-white">
       <Loader2 className="h-16 w-16 animate-spin text-primary" strokeWidth={1} />
     </div>
   );
